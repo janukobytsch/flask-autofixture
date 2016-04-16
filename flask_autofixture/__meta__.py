e30c48a976923ddd60f386b44b509e33a8a8dcda
Janusch Jacoby <janusch.jacoby@student.hpi.uni-potsdam.de>
2016-04-16 03:17:47 +0200

    Initial commit

---
+++ flask_autofixture/__meta__.py
@@ -0,0 +1,16 @@
+"""Project metadata
+"""
+
+__title__ = 'Flask-AutoFixture'
+__summary__ = 'Flask extension which records JSON fixtures right from your test suite'
+__url__ = 'https://github.com/janukobytsch/flask-autofixture'
+
+__version__ = '0.1.0'
+
+__install_requires__ = ['flask']
+__tests_require__ = []
+
+__author__ = 'Janusch Jacoby'
+__email__ = 'janusch.jacoby@web.de'
+
+__license__ = 'MIT License'
---
+++ setup.py
@@ -0,0 +1,45 @@
+"""
+    Flask-AutoFixture
+    ---------------
+    Flask extension which records JSON fixtures right from your test suite.
+
+    Project: https://github.com/janukobytsch/flask-autofixture
+    Documentation: https://github.com/janukobytsch/flask-autofixture
+"""
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+meta = {}
+exec(read('flask_autofixture/__meta__.py'), meta)
+
+setup(
+    name=meta['__title__'],
+    version=meta['__version__'],
+    url=meta['__url__'],
+    license=meta['__license__'],
+    author=meta['__author__'],
+    author_email=meta['__email__'],
+    description=meta['__summary__'],
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=meta['__install_requires__'],
+    tests_require=meta['__tests_require__'],
+    test_suite="tests",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
